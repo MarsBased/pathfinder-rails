b855cea85850e6e821e0f0c10df611a35b1120f1
Xavier Redó <xavier.redo@marsbased.com>
2017-02-03 18:44:28 +0100

    Extracts gitignore into a separate file (#17)

--- recipes/git_ignore.rb
+++ recipes/git_ignore.rb
@@ -3,33 +3,14 @@ module Recipes
 
     def init_file
       @template.remove_file '.gitignore'
-      @template.create_file '.gitignore' do <<-EOF
-        !/log/.keep
-        *.rdb
-        .bundle
-        config/application.yml
-        config/database.yml
-        config/secrets.yml
-        config/secrets.*.yml
-        log/*
-        public/assets
-        public/uploads
-        tmp
-        .DS_Store
-        *.sublime-*
-        .rvmrc
-        stellar.yml
-        .rubocop.yml
-
-        # Ignore generated coverage
-        /coverage
-
-        # Bower stuff
-        vendor/assets/.bowerrc
-        vendor/assets/bower.json
-        vendor/assets/bower_components
-      EOF
-      end
+      @template.create_file '.gitignore', File.open(gitignore_path).read
     end
+
+    private
+
+    def gitignore_path
+      File.join(File.dirname(__FILE__), '..', '.gitignore')
+    end
+
   end
 end
