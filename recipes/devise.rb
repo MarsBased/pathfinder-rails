65508c8929b40a1744e4667ef596d9c0eb1b68ee
David Gómez <david.gomez@marsbased.com>
2017-11-24 10:57:59 +0100

    Fix conflicts

--- recipes/devise.rb
+++ recipes/devise.rb
@@ -15,11 +15,10 @@ module Recipes
 
     def add_development_env_config
       @template.insert_into_file 'config/environments/development.rb', after: "Rails.application.configure do\n" do <<~RUBY
-        \s\sconfig.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
         \s\sconfig.action_controller.asset_host = 'http://localhost:3000'
+        \s\sconfig.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+        \s\sconfig.action_mailer.delivery_method = :letter_opener_web
         \s\sconfig.action_mailer.asset_host = 'http://localhost:3000'
-        \s\sconfig.action_mailer.delivery_method = :smtp
-        \s\sconfig.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
       RUBY
       end
     end
