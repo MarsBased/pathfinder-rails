90f352f282cfa5a90bb83c4d8a3e7260aa66c406
David Gómez <david.gomez@marsbased.com>
2017-11-24 10:50:04 +0100

    Add to routes

--- recipes/devise.rb
+++ recipes/devise.rb
@@ -7,12 +7,28 @@ module Recipes
 
     def init_file
       @template.generate 'devise:install'
-      @template.insert_into_file 'config/environments/development.rb', after: "Rails.application.configure do\n" do <<~RUBY
+      add_development_env_config
+      add_route_config
+    end
+
+    private
+
+    def add_development_env_config
+      @template.insert_into_file 'config/environments/development.rb', after: "Rails.application.configure do\n" do  <<~RUBY
         \s\sconfig.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
         \s\sconfig.action_controller.asset_host = 'http://localhost:3000'
         \s\sconfig.action_mailer.asset_host = 'http://localhost:3000'
         \s\sconfig.action_mailer.delivery_method = :smtp
         \s\sconfig.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+      RUBY
+      end
+    end
+
+    def add_route_config
+      @template.insert_into_file 'config/routes.rb', after: "Rails.application.routes.draw do\n" do  <<~RUBY
+        \s\sif Rails.env.development?
+        \s\s\s\smount LetterOpenerWeb::Engine, at: "/letter_opener"
+        \s\send
       RUBY
       end
     end
