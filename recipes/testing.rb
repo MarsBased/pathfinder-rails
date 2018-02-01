df95e5e49dbe355004a918deb41ccc94a09aadd6
Pablo Rivera Conde <pablo.rivera@marsbased.com>
2018-02-01 21:38:41 +0100

    Add shoulda-matchers

--- recipes/testing.rb
+++ recipes/testing.rb
@@ -7,15 +7,15 @@ module Recipes
 
     def gems
       @template.gem_group :test do |group|
-        group.gem 'rspec-rails'
         group.gem 'factory_bot_rails'
+        group.gem "fakeredis", require: "fakeredis/rspec"
+        group.gem 'rspec-rails'
         group.gem 'simplecov', require: false
+        group.gem 'shoulda-matchers', require: false
         # group.gem 'capybara', require: false
         # group.gem 'capybara-webkit', require: false
         # group.gem 'database_cleaner', require: false
-        # group.gem 'fakeredis', require: false
         # group.gem 'poltergeist', require: false
-        # group.gem 'shoulda-matchers', require: false
       end
 
       @template.gem 'spring-commands-rspec', require: false, group: :development
@@ -25,17 +25,23 @@ module Recipes
       setup_rspec
       setup_factory_bot
       setup_simplecov
+
+      setup_example_tests
     end
 
     private
 
     def setup_rspec
       @template.generate 'rspec:install'
+    end
 
+    def setup_example_tests
       @template.create_file(File.join(*RSPEC_FOLDERS, EXAMPLE_SPEC_FILE)) do |file|
         <<~RSPEC
           RSpec.describe 'Specs' do
-            it { true == true }
+            context 'rspec' do
+              it { true == true }
+            end
           end
         RSPEC
       end
