a26124d39b8db9b3908bdbb953b4850c2022d8eb
Juan Artero <juan.artero@marsbased.com>
2019-11-07 09:05:18 +0100

    Set PostgreSQL as default database
    We always use PostgreSQL in our projects, make sense that this was the default database

--- recipes/database.rb
+++ recipes/database.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 module Recipes
   class Database < Base
-
     is_auto_runnable
 
     def gems
       case @template.options.database
       when 'postgresql'
-        @template.gem 'pg', '~> 0.18'
+        @template.gem 'pg'
       when 'mysql'
         @template.gem 'mysql2', '>= 0.3.18'
-      else
+      when 'sqlite3'
         @template.gem 'sqlite3'
+      else
+        @template.gem 'pg'
       end
     end
 
     def cook
       @template.rake 'db:create'
     end
-
   end
 end
